f6a944170279b7346a70a18881a48ae90b0da1c2
jennifermorinigo <jennifermorinigo@outlook.com>
2022-05-03 21:56:37 -0300

    segunda subida

---
+++ primerreact/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+export default function Navbar() {
+    
+    const headerStyles = {
+        display: "flex",
+        flexWrap: "wrap",
+        jusifyContent: "space-between",
+        listStyle: "none"
+    }
+    return (
+        <header className="header">
+            <div className="nombre">
+                SKZ
+            </div>
+            <ul className="links" style={headerStyles}>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Quienes somos</li>
+            </ul>
+        </header>
+    );
+}
